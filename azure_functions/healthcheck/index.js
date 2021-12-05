bd565d3279d6fd516a8b12e3cae3ef08dd13031d
Martin Belyanov <mbelianov@gmail.com>
2021-12-05 19:26:42 +0200

    daily commit

--- azure_functions/healthcheck/index.js
+++ azure_functions/healthcheck/index.js
@@ -1,9 +1,16 @@
-
+const bodimed = require('../common/bodimed_connect');
 
 module.exports = async function (context, req) {
   context.log('healthcheck function processed a request.');
 
-
+  let patients = await bodimed.getPatients(context, "9002215792", "egn");
+  let results = []
+  for (let patient of patients.patientsList){
+    let result = await bodimed.getResults(context, `?idnap=${patient.bodimed_patient_id}&pass=${patient.bodimed_patient_password}`)
+    results.push(result)
+  }
+  context.log (results[0].outcome)
+  
   const name = (req.query.name || (req.body && req.body.name));
   const responseMessage = name
     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
